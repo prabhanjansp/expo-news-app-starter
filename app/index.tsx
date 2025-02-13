6c31d5cc50d598c33617e8264633f54291dd1e6d
prabhanjansp <puranikchetan97@gmail.com>
2025-02-13 23:24:40 +0530

    namma news app

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,32 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
 import { useRouter } from "expo-router";
+import { Colors } from "@/constants/Colors";
 
 const Page = () => {
   const router = useRouter();
 
   return (
     <View style={styles.container}>
-      <Text>Welcome Page</Text>
-      <TouchableOpacity onPress={() => router.replace("/(tabs)")}>
-        <Text>Go to Home Screen</Text>
-      </TouchableOpacity>
+      <ImageBackground
+        source={require("@/assets/images/news.jpg")}
+        style={{ height: "100%", width: "100%",flex:1}}
+        resizeMode="cover"
+      >
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>News Nammalle</Text>
+        <Text style={styles.description}>Get breaking news and personalized updates directly to you feed</Text>
+        <TouchableOpacity style={styles.btn} onPress={() => router.replace("/(tabs)")}>
+          <Text style={styles.btnText}>Get Started!</Text>
+        </TouchableOpacity>
+      </View>
+      </ImageBackground>
     </View>
   );
 };
@@ -23,4 +39,40 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  wrapper: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingBottom: 50,
+    paddingHorizontal: 30,
+    gap: 10,
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  title:{
+    color: Colors.white ,
+    fontSize:24,
+    fontWeight:'600',
+    letterSpacing:1.5,
+    textAlign: "center",lineHeight: 36
+  },
+  description:{
+    color: Colors.white ,
+    fontSize:16,
+    fontWeight:'500',
+    letterSpacing:1.2,
+    textAlign: "center",lineHeight: 22
+  },
+  btn:{
+    backgroundColor:Colors.tint,
+    paddingVertical:15,
+    marginVertical:20,
+    borderRadius:10,
+    alignItems: "center",
+
+  },
+  btnText:{
+    color: Colors.white ,
+    fontSize:16,
+    fontWeight:'700',
+
+  }
 });
